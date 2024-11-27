9ca038dd97422d73f731f521ee124a98c0add84c
alish <alifshania222@gmail.com>
2024-11-27 21:56:13 +0330

    finish first part of array statements

--- Arrays.php
+++ Arrays.php
@@ -1,13 +1,36 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+
 <body>
 
+
+    <?php
+    $users = ["alireza", "mobina", "saeed", "fatemeh"];
+    // $users2 = array("alireza", "mobina", "saeed", "fatemeh");
+    // var_dump($users[0])
+
+
+    // $user = [
+    //     'name' => 'iman',
+    //     'age' => 40,
+    //     'color' => 'white'
+    // ];
+
+    // var_dump(count($users));
+    // var_dump(sizeof($users));
+    var_dump(in_array('iman', $users))
+
+    ?>
+
+
 </body>
+
 </html>
