cb9a0f23871c9482867a03939570f7a4e4480483
alish <alifshania222@gmail.com>
2024-11-27 20:40:24 +0330

    first commit

---
+++ start.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+
+
+
+<?php
+    $name = "ali afshani";
+    $age = 34;
+    $temp = 43.7;
+    $list = ["ali" , 'iman' , true];
+
+    print_r($list);
+
+    echo $name;
+    echo $age;
+?>
+
+</body>
+</html>
