32aa370372323f6a2f2a42d33100acfd41f99a91
alish <alifshania222@gmail.com>
2024-11-28 19:45:56 +0330

    loops in php

--- IFandELSE.php
+++ IFandELSE.php
@@ -1,1 +1,10 @@
 <?php
+$age = 16;
+//$gender = 'male';
+//if ($age >= 18 && $gender ==='femail') {
+//    echo 'can enter';
+//}else{
+//    echo 'can not enter';
+//}
+
+//echo($age >= 18 ? 'can enter' : 'can not enter');
---
+++ for_loop.php
@@ -0,0 +1,7 @@
+<?php
+$users = ["ali" ,'iman' , 'mamad' , 'nima'];
+
+for ($i = 0; $i < count($users); $i++) {
+    echo "<h2>$users[$i]</h2>";
+
+}
